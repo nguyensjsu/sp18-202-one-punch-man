f63664211633c05c8c4990627cd408678bfd804c
ZhiyeChen <zhiye.chen@sjsu.edu>
2018-04-17 03:43:27 -0700

    .

--- Knowledge_Is_Power/BaseWorld.java
+++ Knowledge_Is_Power/BaseWorld.java
@@ -30,13 +30,13 @@ public class BaseWorld extends World
     public void prepare()
     {
         /* create player */
-        Player player = new DrP();  //size 50*50
+        Player player = new Tesla();  //size 50*50
         addObject(player, 800, 400);
         HpDecorator player_hp = new HpDecorator(player,100,100,0,40,50,10);   // hp 100-100, offset(0,40), size 50*10
         addObject(player_hp, 10000, 10000);
         
         /* create 3 chasing enermies that shots bullets */
-        for (int i=0; i<1; i++){
+        for (int i=0; i<4; i++){
             Enermy enermy = new TestEnermy(50,50,"chase","bullet");
             addObject(enermy, (int)(1600*random()), (int)(900*random()));
             HpDecorator enermy_hp = new HpDecorator(enermy,20,20,0,40,50,10);   //hp 20-20, offset(0,40), size 50*10
