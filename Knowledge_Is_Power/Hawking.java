44246825c84f080af1f3f4eaa5b9f89817a259b6
daisydanlu <daisydanlu@gmail.com>
2018-04-19 23:55:33 -0700

    Set timer for Hawking skills

--- Knowledge_Is_Power/Hawking.java
+++ Knowledge_Is_Power/Hawking.java
@@ -13,16 +13,20 @@ public class Hawking extends Player
      * Act - do whatever the Hawking wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    private int attack_speed = 30;  //2 per sec
-    private int attack_timer = 0;
-    private int blackhole_duration_time = 0;
-    private int blackhole_cd_time = 0;
-    private int movefast_duration_time = 0;
-    private int movefast_cd_time = 0;
-    private int explored_duration_time = 0;
-    private int explored_cd_time = 0;
-    private int move_speed = 30;
-    private String player_image = "man01.png";
+   
+    protected String player_image = "man01.png";
+   
+    protected int attack_speed = 15;
+    protected int bullet_damage = 5;
+    
+    /* timer */
+    protected int skill_one_duration_timer = 0;
+    protected int skill_one_cd_timer = 0;
+    protected int skill_two_duration_timer = 0;
+    protected int skill_two_cd_timer = 0;
+    protected int ult_duration_timer = 0;
+    protected int ult_cd_timer = 0;
+    private int move_speed = 300;
 
     
     public Hawking() {
@@ -34,53 +38,46 @@ public class Hawking extends Player
         size_y = y;
         GreenfootImage image = getImage();
         image.scale(size_x, size_y);
-        setImage(image);      
+        setImage(image);     
     }
     
     public void act() 
     {
        /* update move */
-       if (move_state != "freeze"){
+       if (!freeze_state){
            
            switch (move_state){
                case "wasd": wasd_move(); break;
                case "push": push(push_x, push_y, push_speed); break;
                default: break;
             }
-          /* invincible flash */
-           //invincible_flash(player_image,trans_image);
+           
+            
+           
+
+           /* ability */
            base_attack();
-           if(Greenfoot.isKeyDown("1")){
-               
-               if(blackhole_cd_time == 0 && blackhole_duration_time == 0) {
-                   blackHoleAttack();
-               }
-               
-               blackHoleTimer();
-            }
-            else if(Greenfoot.isKeyDown("2")){
-                moveFast();
-            }
-            else if(Greenfoot.isKeyDown("3")){
-                explodeAttack();
-            }
-           timer();
+           skill_one();
+           skill_two();
+           ult();
+           
+           /* invincible flash */
+           //invincible_flash(player_image,trans_image);
+           
+             /* timer */
+           timer(); 
+           additional_timer();
    
        }
        
-      /* game over condition */
-      dead();
+      /* animation timer */
+      // animation_timer();
+       
+       /* game over condition */
+       dead();
 
      } 
-     
-   /* base attack */
-    public void base_attack(){
-        MouseInfo mouse = Greenfoot.getMouseInfo();
-        /* bullet attack, fire by mouse */
-        if (mouse != null && Greenfoot.mousePressed(null)){
-            bullet_attack();
-        }
-    }
+    
     
     /* bullet style attack (aim by mouse)*/
     public void bullet_attack(){
@@ -89,68 +86,82 @@ public class Hawking extends Player
             attack_timer = attack_speed;
         }
     }
-     
- /* movement control using key 2 */
-    public void moveFast()
-    {
-        int update_x = getX();
-        int update_y = getY();
-
-        MouseInfo mouse = Greenfoot.getMouseInfo();
-       if (mouse != null){
-           turnTowards(mouse.getX(),mouse.getY());
-       }
-      
-        /* update */
-        update_x = (int)(getX() + move_speed*cos(toRadians(getRotation())));
-        update_y = (int)(getY() + move_speed*sin(toRadians(getRotation())));
-
-        /* move */
-        setLocation(update_x,update_y);
-        
-        /* observer notify */
-        for (Enermy enermy: getWorld().getObjects(Enermy.class)){
-            enermy.update(update_x,update_y);
+    
+    
+    public void skill_one(){
+        if (skill_one_cd_timer == 0){
+            if(Greenfoot.isKeyDown("1")){
+              
+                //MouseInfo mouse = Greenfoot.getMouseInfo();
+                 int update_x = (int)(getX() + 200*cos(toRadians(getRotation())));
+                 int update_y = (int)(getY() + 200*sin(toRadians(getRotation())));
+                 getWorld().addObject(new BlackHoleDecorator(100,100,bullet_damage),update_x, update_y);
+                
+              
+                skill_one_cd_timer = 360;
+            }
         }
     }
     
-  /* movement control using key 1 */  
-   public void blackHoleAttack() 
-   {
-     
-     MouseInfo mouse = Greenfoot.getMouseInfo();
-     int update_x = (int)(getX() + 200*cos(toRadians(getRotation())));
-     int update_y = (int)(getY() + 200*sin(toRadians(getRotation())));
-     getWorld().addObject(new BlackHole(getRotation(), 80, 80, 1),update_x, update_y);
-     
-     
-   }
-   
-   public void explodeAttack()
-   {
-     MouseInfo mouse = Greenfoot.getMouseInfo();
-     int update_x = (int)(getX() + 200*cos(toRadians(getRotation())));
-     int update_y = (int)(getY() + 200*sin(toRadians(getRotation())));
-     getWorld().addObject(new BlackHole(getRotation(), 80, 80, 1),update_x, update_y);
-     for (Enermy enermy: getWorld().getObjects(Enermy.class)){
-        enermy.setLocation(update_x, update_y);
-        enermy.damage(getX(),getY(),1, "bullet");
+    public void skill_two(){
+        if (skill_two_cd_timer == 0){
+            if(Greenfoot.isKeyDown("2")){
+               
+               MouseInfo mouse = Greenfoot.getMouseInfo();
+               if (mouse != null){
+                   turnTowards(mouse.getX(),mouse.getY());
+               }
+              
+                /* update */
+               int update_x = (int)(getX() + move_speed*cos(toRadians(getRotation())));
+               int update_y = (int)(getY() + move_speed*sin(toRadians(getRotation())));
+        
+                /* move */
+                setLocation(update_x,update_y);
+                
+                /* observer notify */
+                for (Enermy enermy: getWorld().getObjects(Enermy.class)){
+                    enermy.update(update_x,update_y);
+                }
+                
+                skill_two_cd_timer = 360;
+            }
+        }
+    }
+    
+    public void ult(){
+        if (ult_cd_timer == 0){
+            if(Greenfoot.isKeyDown("3")){
+                MouseInfo mouse = Greenfoot.getMouseInfo();
+                 int update_x = (int)(getX() + 200*cos(toRadians(getRotation())));
+                 int update_y = (int)(getY() + 200*sin(toRadians(getRotation())));
+                 getWorld().addObject(new BlackHoleDecorator(200, 200, bullet_damage),update_x, update_y);
+                 for (Enermy enermy: getWorld().getObjects(Enermy.class)){
+                        enermy.setLocation(getX(), getY());
+                        enermy.damage(getX(),getY(),bullet_damage, "bullet");
+                    
+                 }
+               
+                 
+                 ult_cd_timer = 360;
+                
+                
+            }
+        }
+    }
+    
+    public void additional_timer(){
+    
+        if (skill_one_cd_timer != 0){skill_one_cd_timer--;}
+        
+        if (skill_two_cd_timer != 0){skill_two_cd_timer--;}
+        
+        if (ult_cd_timer != 0){ult_cd_timer--;}
         
     }
-
-   }
     
-
-    public void timer(){   
-       if (attack_timer != 0) attack_timer--;
-       if (push_timer != 0) push_timer--;
-       if (invincible_timer != 0) invincible_timer--;
-    }
+   
     
-    public void blackHoleTimer(){   
-       if (blackhole_duration_time != 0) blackhole_duration_time--;
-       if (blackhole_cd_time != 0) blackhole_cd_time--;
-       
-    }
-     
+    
+    
 }
