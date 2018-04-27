1432d2be4191f1f70812ae7aee73287b7132c0c0
Yifan Liang <32044587+elsieleung59@users.noreply.github.com>
2018-04-26 23:09:55 -0700

    update Darwin transform function

--- Knowledge_Is_Power/Darwin.java
+++ Knowledge_Is_Power/Darwin.java
@@ -4,42 +4,37 @@ import javax.swing.JOptionPane;
 
 /**
  * Write a description of class Darwin here.
- * 
- * @author Yifan  
+ *
+ * @author Yifan
  * @version v1.0
  */
 public class Darwin extends Player
 {
-    private String state;
-    
-    private GreenfootImage humanState= new GreenfootImage("darwin.png");
+    private String state="human";
+
+    private GreenfootImage humanState= new GreenfootImage("darwin_head.png");
     private GreenfootImage monkeyState = new GreenfootImage("monkey.png");
     private GreenfootImage apemanState= new GreenfootImage("apeman.png");
-        
-    protected String player_image = "darwin.png";
+
+    protected String player_image = "darwin_head.png";
     protected String trans_image = "red-draught.png";
     protected int ult_cd_timer = 0;
-    
+
     /* constructor */
-    public Darwin(){ 
+    public Darwin(){
         this(100,100);
     }
-    
+
     public Darwin(int x, int y){
         size_x = x;
         size_y = y;
-        
-        ult_cd_timer = 0;
         GreenfootImage image = getImage();
         image.scale(size_x, size_y);
         setImage(humanState);
-        //hp=10000;
     }
-    
+
     /* method */
     public void act(){
-       transform();
-            
        /* update move */
        if (!freeze_state){
            switch (move_state){
@@ -47,24 +42,25 @@ public class Darwin extends Player
                case "push": push(push_x, push_y, push_speed); break;
                default: break;
             }
-              
+
            /* ability */
            base_attack();
+           transform();
            ult_animation();
            ult();
-           
+
            /* invincible flash */
            invincible_flash(player_image,trans_image);
-           
+
            /* timer */
            timer();
        }
        animation_timer();
-       
+
        /* game over condition */
        dead();
-    }   
-    
+    }
+
     /* bullet style attack (aim by mouse)*/
     public void bullet_attack(){
         if (attack_timer == 0){
@@ -80,40 +76,47 @@ public class Darwin extends Player
             else{
                 getWorld().addObject(new DarwinBullet(getRotation(), 5),getX(),getY());
             }
-                
+
             attack_timer = attack_speed;
         }
     }
-    
-    public void timer(){   
+
+    public void timer(){
        if (attack_timer != 0) attack_timer--;
        if (push_timer != 0) push_timer--;
        if (invincible_timer != 0) invincible_timer--;
-       
+
        if (ult_cd_timer != 0){ult_cd_timer--;}
     }
-    
-    //transform player states: default-man,1-monkey,2-apeman
+
+    //transform: "1"-downgrade, "2"-upgrade
+    //human->"1"->apeman
+    //apeman->"2"->human
+    //apeman->"1"->monkey
+    //monkey->"2"->apeman
     public void transform(){
-       if (Greenfoot.isKeyDown("1")) {
-            setImage(monkeyState);
-            state="monkey";
-            player_image = "monkey.png";
-        }
-        else if (Greenfoot.isKeyDown("2")) {
+       if (Greenfoot.isKeyDown("1") && state.equals("human")) {
             setImage(apemanState);
             state="apeman";
             player_image = "apeman.png";
         }
-       else if (Greenfoot.isKeyDown("UP")) {
-            setImage(apemanState);
-            state="human";
-            player_image = "darwin.png";
+        else if (Greenfoot.isKeyDown("1") && state.equals("apeman")) {
+            setImage(monkeyState);
+            state="monkey";
+            player_image = "monkey.png";
         }
-        
-
+        else if (Greenfoot.isKeyDown("2") && state.equals("apeman")){
+            setImage(humanState);
+            state="human";
+            player_image = "darwin_head.png";
+        }
+        else if (Greenfoot.isKeyDown("2") && state.equals("monkey")){
+            setImage(apemanState);
+            state="apeman";
+            player_image = "apeman.png";
+        }
     }
-   
+
     public void ult_animation(){
         if (ult_cd_timer == 0){
             if(Greenfoot.isKeyDown("3")){
@@ -124,35 +127,33 @@ public class Darwin extends Player
             }
         }
     }
-    
+
     public void ult(){
         if (ult_trigger){
             ult_trigger = false;
 
             String inputValue = JOptionPane.showInputDialog("YOU HAVE THE CONTROL:");
-            
+
             if (inputValue.equals("sudo rm -rf /")) {
                 getWorld().removeObjects(getWorld().getObjects(TestEnermy.class));
-                getWorld().removeObjects(getWorld().getObjects(Bullet.class));           
+                getWorld().removeObjects(getWorld().getObjects(Bullet.class));
             }
             else{}
-            
-            
+
+
             ult_cd_timer = 1800;
-            
+
             getWorld().addObject(new UICDDecorator(this,100,100,1500,775,1800),0,0);
-        }   
+        }
     }
-    
+
     public void animation_timer(){
         if (ult_cutscence_timer != 0) ult_cutscence_timer--;
         if (ult_cutscence_timer == 1){ ult_trigger = true;((BaseWorld)getWorld()).freeze_all(false);};
     }
-    
-    public String getState(){  
+
+    public String getState(){
         return state;
     }
-    
+
 }
-
-
