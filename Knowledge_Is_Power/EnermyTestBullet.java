6ffcd07a86bc3d1df0eb5d09cbeb2e384393313d
WhiteKaras <fuhe1234@gmail.com>
2018-04-30 00:37:01 -0700

    0Hp bug fixed, boom effect layer modified

--- Knowledge_Is_Power/EnermyTestBullet.java
+++ Knowledge_Is_Power/EnermyTestBullet.java
@@ -10,5 +10,9 @@ public class EnermyTestBullet extends EnermyBullet
 {
     public EnermyTestBullet(int r){
         super(r,20,20,1);        //default size 20*20, not through, 1 damage
+    
+        GreenfootImage image = new GreenfootImage("laser.png");
+        image.scale(size_x, size_y);
+        setImage(image);
     }
 }
